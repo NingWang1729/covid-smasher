98c365da4a09d08f7b09d67a1e1f4b8f6c1253ea
Ning Wang <ningwang1729@gmail.com>
2021-03-22 16:41:05 -0700

    added basic template for canvas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import './base_classes/locations.js';
 import './base_classes/timezones.js';
 import './base_classes/player.js';
+import { useRef } from 'react';
 
 function App() {
+  const canvasRef = useRef(null);
+  // Use 24hr clock, for easy modding
+  const [time, setTime] = useState(0);
+
+  function pass_time(time_passed) {
+    setTime((time + time_passed) % 24);
+  };
+
+  // Initializes display screen
+  useEffect(()=>{
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+    ctx.fillStyle = "blue";
+    ctx.fillRect(10, 10, 200, 100);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Don't Learn React yet
-        </a>
+        <h1>
+          Covid-smasher
+        </h1>
+        <br />
+        <table id="game-table">
+          <tr>
+            <td id="left-column">
+              <p>Left column</p>
+            </td>
+            <td id="center-column">
+              <canvas ref={canvasRef} width="1024" height="768" id="game-canvas"/>
+            </td>
+            <td id="right-column">
+              <p>Right column</p>
+            </td>
+          </tr>
+        </table>
       </header>
     </div>
   );
