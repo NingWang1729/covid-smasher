1362f8694b03cda86902341f19684f9dc8a3222c
yao-alan <alanyao314@gmail.com>
2021-03-23 19:03:40 -0700

    Creating branch

--- src/base_classes/player.js
+++ src/base_classes/player.js
@@ -50,4 +50,79 @@ class player {
     };
 };
 
+class Role extends player {
+    constructor(x_pos, y_pos, hp, cash, strength, intelligence, morale, type) {
+        super(x_pos, y_pos);
+        this._hp = hp;
+        this._cash = cash;
+        this._strength = strength;
+        this._intelligence = intelligence;
+        this._morale = morale;
+        this._type = type;
+    }
+    get type() {
+        return this._type;
+    }
+    get strength() {
+        return this._strength;
+    }
+    get intelligence() {
+        return this._intelligence;
+    }
+    get morale() {
+        return this._morale;
+    }
+    set delta_health(val) {
+        this._hp += val;
+    }
+    set delta_cash(val) {
+        this._cash += val;
+    }
+    set delta_strength(val) {
+        this._strength += val;
+    }
+    set delta_intelligence(val) {
+        this._intelligence += val;
+    }
+    set delta_morale(val) {
+        this._morale += val;
+    }
+};
+
+class HSTeen extends Role {
+    constructor(x_pos, y_pos) {
+        super(x_pos, y_pos, 100, 100, 40, 45, 50, 'HSTeen');
+    }
+};
+
+class CollegeStudent extends Role {
+    constructor(x_pos, y_pos) {
+        super(x_pos, y_pos, 100, 200, 50, 69, 50, 'CollegeStudent');
+    }
+};
+
+class PoorPerson extends Role {
+    constructor(x_pos, y_pos) {
+        super(x_pos, y_pos, 100, 50, 30, 50, 30, 'PoorPerson');
+    }
+};
+
+class RichKid extends Role {
+    constructor(x_pos, y_pos) {
+        super(x_pos, y_pos, 100, 1000, 20, 60, 60, 'RichKid');
+    }
+};
+
+class OldMan extends Role {
+    constructor(x_pos, y_pos) {
+        super(x_pos, y_pos, 100, 100, 10, 50, 40, 'OldMan');
+    }
+};
+
+class Pedestrian extends player {
+    constructor(x_pos, y_pos) {
+        super(x_pos, y_pos);
+    }
+}
+
 export { player };
