4cfd60467ef164ea1cdb51959819b1a1c1de8bc5
yao-alan <alanyao314@gmail.com>
2021-03-23 20:17:50 -0700

    Improved movement

--- src/covid_smasher.js
+++ src/covid_smasher.js
@@ -7,6 +7,11 @@ const timezones_module = require('./base_classes/timezones.js');
 const player_module = require('./base_classes/player.js');
 
 const player = new player_module.player(2, 5);
+var animated = false;
+var animation_stage = 0;
+
+var lastInputTick = 0;
+
 function COVID_SMASHER() {
     // CANVAS WIDTH
     const MAX_WIDTH = 1280;
@@ -22,6 +27,8 @@ function COVID_SMASHER() {
     const [time, setTime] = useState(0);
     // Movequeue for storing keyboard inputs
     const [moves, setMoves] = useState([]);
+    // Game state
+    const [game_state, setGameState] = useState(0);
     
     // Initializes display screen
     useEffect(()=>{
@@ -31,61 +38,228 @@ function COVID_SMASHER() {
 
     // Game Clock
     useEffect(()=>{
-        setTimeout(counter, 100);
+        setTimeout(counter, 20);
     }, [ticks]);
 
     // Game Clock
     function counter () {
-        update_game();
+        switch (game_state) {
+            case 0:
+                update_game_0();
+                break;
+            case 1:
+                update_game_1();
+                break;
+            default:
+                update_game_0();
+                break;
+        }
         setTicks(ticks + 1)
     }
 
-    function update_game () {
+    // WORLD MAP
+    function update_game_0 () {
         let movequeue = moves;
-        switch (movequeue[0]) {
-            case 0:
-                if (player.direction === locations_module.DIRECTION.LEFT) {
-                    player.move_left();
-                } else {
-                    player.set_direction(locations_module.DIRECTION.LEFT);
-                };
-                break;
-            case 1:
-                if (player.direction === locations_module.DIRECTION.RIGHT) {
-                    player.move_right();
-                } else {
-                    player.set_direction(locations_module.DIRECTION.RIGHT);
-                };
-                break;
-            case 2:
-                if (player.direction === locations_module.DIRECTION.UP) {
-                    player.move_up();
-                } else {
-                    player.set_direction(locations_module.DIRECTION.UP);
-                };
-                break;
-            case 3:
-                if (player.direction === locations_module.DIRECTION.DOWN) {
-                    player.move_down();
-                } else {
-                    player.set_direction(locations_module.DIRECTION.DOWN);
-                };
-                break;
-            default:
-                break;
+        if (moves.length > 0 && ticks % 2 === 0) {
+            switch (movequeue[0]) {
+                case 0:
+                    if (player.direction === locations_module.DIRECTION.LEFT && player.x_pos > 0 && locations_module.WORLD_MAP[player.y_pos][player.x_pos - 1] === 0) {
+                        animated = true;
+                    } else {
+                        animation_stage = 0;
+                        animated = false;
+                    };
+                    break;
+                case 1:
+                    if (player.direction === locations_module.DIRECTION.RIGHT && player.x_pos < locations_module.WORLD_WIDTH - 1 && locations_module.WORLD_MAP[player.y_pos][player.x_pos + 1] === 0) {
+                        animated = true;
+                    } else {
+                        animation_stage = 0;
+                        animated = false;
+                    };
+                    break;
+                case 2:
+                    if (player.direction === locations_module.DIRECTION.UP && player.y_pos > 0 && locations_module.WORLD_MAP[player.y_pos - 1][player.x_pos] === 0) {
+                        animated = true;
+                    } else {
+                        animation_stage = 0;
+                        animated = false;
+                    };
+                    break;
+                case 3:
+                    if (player.direction === locations_module.DIRECTION.DOWN && player.y_pos < locations_module.WORLD_HEIGHT - 1 && locations_module.WORLD_MAP[player.y_pos + 1][player.x_pos] === 0) {
+                        animated = true;
+                    } else {
+                        animation_stage = 0;
+                        animated = false;
+                    };
+                    break;
+                default:
+                    break;
+            };
         };
-        movequeue.shift();
-        setMoves(movequeue);
-
         const canvas = canvasRef.current;
         const ctx = canvas.getContext("2d");
-        ctx.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT);
+        // Clears entire canvas
+        ctx.clearRect(0, 0, MAX_WIDTH, MAX_HEIGHT + locations_module.TOP_BUFFER);
+
+        // Shadow of the buildings
+        ctx.fillStyle = "blue";
+        ctx.fillRect(0 *  locations_module.UNIT_SIZE, 0 * locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 40 * locations_module.UNIT_SIZE, 4 * locations_module.UNIT_SIZE);
+        ctx.fillRect(0 *  locations_module.UNIT_SIZE, 8 * locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 5 * locations_module.UNIT_SIZE, 5 * locations_module.UNIT_SIZE);
+        ctx.fillRect(9 *  locations_module.UNIT_SIZE, 7 * locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 10 * locations_module.UNIT_SIZE, 7 * locations_module.UNIT_SIZE);
+        ctx.fillRect(22 *  locations_module.UNIT_SIZE, 7 * locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 18 * locations_module.UNIT_SIZE, 4 * locations_module.UNIT_SIZE);
+        ctx.fillRect(0 *  locations_module.UNIT_SIZE, 17 * locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 7 * locations_module.UNIT_SIZE, 7 * locations_module.UNIT_SIZE);
+        ctx.fillRect(12 *  locations_module.UNIT_SIZE, 17 * locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 13 * locations_module.UNIT_SIZE, 4 * locations_module.UNIT_SIZE);
+        ctx.fillRect(25 *  locations_module.UNIT_SIZE, 17 * locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 15 * locations_module.UNIT_SIZE, 7 * locations_module.UNIT_SIZE);
+        
+
+        let home = document.getElementById("home");
+        ctx.drawImage(home, -0, -48 + locations_module.TOP_BUFFER);
+
+        let neighbor = document.getElementById("neighbor");
+        ctx.drawImage(neighbor, 128, -16 + locations_module.TOP_BUFFER);
+
+        let city_hall = document.getElementById("city-hall");
+        ctx.drawImage(city_hall, 256, -56 + locations_module.TOP_BUFFER);
+
         ctx.fillStyle = "green";
         ctx.beginPath();
         ctx.arc(((player.get_x_pos() * locations_module.UNIT_SIZE) + (locations_module.UNIT_SIZE / 2)),
-                ((player.get_y_pos() * locations_module.UNIT_SIZE) + (locations_module.UNIT_SIZE / 2)),
+                ((player.get_y_pos() * locations_module.UNIT_SIZE) + (locations_module.UNIT_SIZE / 2) + locations_module.TOP_BUFFER),
                 (locations_module.UNIT_SIZE / 2), 0, 2 * Math.PI);
         ctx.fill();
+        draw_sprite(ctx, player.direction);
+        
+        
+        if (moves.length > 0 && ticks % 2 === 0) {
+            switch (movequeue[0]) {
+                case 0:
+                    if (player.direction === locations_module.DIRECTION.LEFT) {
+                        player.move_left();
+                    } else {
+                        player.set_direction(locations_module.DIRECTION.LEFT);
+                    };
+                    break;
+                case 1:
+                    if (player.direction === locations_module.DIRECTION.RIGHT) {
+                        player.move_right();
+                    } else {
+                        player.set_direction(locations_module.DIRECTION.RIGHT);
+                    };
+                    break;
+                case 2:
+                    if (player.direction === locations_module.DIRECTION.UP) {
+                        player.move_up();
+                    } else {
+                        player.set_direction(locations_module.DIRECTION.UP);
+                    };
+                    break;
+                case 3:
+                    if (player.direction === locations_module.DIRECTION.DOWN) {
+                        player.move_down();
+                    } else {
+                        player.set_direction(locations_module.DIRECTION.DOWN);
+                    };
+                    break;
+                default:
+                    break;
+            };
+            movequeue.shift();
+            setMoves(movequeue);
+        };
+    }
+
+    // PAUSE MENU
+    function update_game_1() {
+        const canvas = canvasRef.current;
+        const ctx = canvas.getContext("2d");
+        // Clears most of the canvas
+        ctx.clearRect(locations_module.UNIT_SIZE, locations_module.UNIT_SIZE, MAX_WIDTH - 2 * locations_module.UNIT_SIZE, MAX_HEIGHT + locations_module.TOP_BUFFER - 2 * locations_module.UNIT_SIZE);
+        ctx.fillStyle = "beige";
+        ctx.fillRect(locations_module.UNIT_SIZE, locations_module.UNIT_SIZE, MAX_WIDTH - 2 * locations_module.UNIT_SIZE, MAX_HEIGHT + locations_module.TOP_BUFFER - 2 * locations_module.UNIT_SIZE);
+        ctx.font = '48px serif';
+        ctx.fillStyle = "black";
+        ctx.fillText("PAUSED", MAX_WIDTH / 2 - 2 * locations_module.UNIT_SIZE, MAX_HEIGHT / 4 - 2 * locations_module.UNIT_SIZE, 4 * locations_module.UNIT_SIZE, 2 * locations_module.UNIT_SIZE);
+    };
+
+    // Sprite drawer
+    function draw_sprite(ctx, direction) {
+        let sprite_sheet = document.getElementById("player-sprite-sheet");
+        switch (direction) {
+            case locations_module.DIRECTION.UP:
+                draw_animation(ctx, 1 + animation_stage);
+                break;
+            case locations_module.DIRECTION.DOWN:
+                draw_animation(ctx, 4 + animation_stage);
+                break;
+            case locations_module.DIRECTION.LEFT:
+                draw_animation(ctx, 7 + animation_stage);
+                break;
+            case locations_module.DIRECTION.RIGHT:
+                draw_animation(ctx, 10 + animation_stage);
+                break;
+            default:
+                break;
+        }
+        if (moves.length === 0) {
+            animation_stage = 0
+            animated = false;
+        }
+        if (animated) {
+            if (animation_stage === 1) {
+                animation_stage = 2;
+            } else {
+                animation_stage = 1;
+            }
+        }
+    }
+
+    // For draw_sprite()
+    function draw_animation(ctx, sprite_no) {
+        let sprite_sheet = document.getElementById("player-sprite-sheet");
+        switch(sprite_no) {
+            // Up
+            case 1:
+                ctx.drawImage(sprite_sheet, 0, 0, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            case 2:
+                ctx.drawImage(sprite_sheet, 128, 0, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER + 16, 64, 64);
+                break;
+            case 3:
+                ctx.drawImage(sprite_sheet, 64, 192, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            // Down
+            case 4:
+                ctx.drawImage(sprite_sheet, 128, 64, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            case 5:
+                ctx.drawImage(sprite_sheet, 128, 192, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER - 16, 64, 64);
+                break;
+            case 6:
+                ctx.drawImage(sprite_sheet, 128, 128, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            // Left
+            case 7:
+                ctx.drawImage(sprite_sheet, 0, 128, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            case 8:
+                ctx.drawImage(sprite_sheet, 0, 64, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2 + 16, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            case 9:
+                ctx.drawImage(sprite_sheet, 0, 192, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            // Right
+            case 10:
+                ctx.drawImage(sprite_sheet, 64, 0, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            case 11:
+                ctx.drawImage(sprite_sheet, 64, 128, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2 - 16, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+            case 12:
+                ctx.drawImage(sprite_sheet, 64, 64, 64, 64, player.get_x_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE / 2, player.get_y_pos() * locations_module.UNIT_SIZE - locations_module.UNIT_SIZE + locations_module.TOP_BUFFER, 64, 64);
+                break;
+        }
     }
 
     // To increment in-game time
@@ -95,7 +269,15 @@ function COVID_SMASHER() {
 
     // Keyboard inputs
     document.onkeydown = function(e) {
+        if (ticks < lastInputTick + 10) {
+            return;
+        } else {
+            lastInputTick = ticks;
+        }
         var movequeue = moves;
+        if (moves.length >= 1) {
+            return;
+        }
         switch(e.which) {
             case 37: // Left
             case 65: // A
@@ -132,8 +314,10 @@ function COVID_SMASHER() {
                 break;
             case 80: // P
                 e.preventDefault();
-                if (play) {
+                if (game_state === 0) {
+                    setGameState(1);
                 } else {
+                    setGameState(0);
                 }
                 setPlay(!play);
                 break;
@@ -145,20 +329,31 @@ function COVID_SMASHER() {
 
     return (
         <table id="game-table">
-          <tr>
-            <td id="left-column">
-              <p>Left column</p>
-              <p>{ticks}</p>
-              <p>{player.direction}</p>
-              <p>({player.x_pos},{player.y_pos})</p>
-            </td>
-            <td id="center-column">
-              <canvas ref={canvasRef} width={MAX_WIDTH} height={MAX_HEIGHT} id="game-canvas"/>
-            </td>
-            <td id="right-column">
-              <p>Right column</p>
-            </td>
-          </tr>
+            <tr>
+                <td id="left-column">
+                    <p>Left column</p>
+                    <p>{ticks}</p>
+                    <p>{player.direction}</p>
+                    <p>({player.x_pos},{player.y_pos})</p>
+                    {/* This Cynthia is in public */}
+                    <img src={player.img.src} alt="Cynthia not here wtf" id="player-sprite-sheet" style={{visibility: 'hidden'}}></img>
+                    {/* Top row buildings */}
+                    <img src="/images/homes/Apartment.png" alt="Cynthia not here wtf" id="home" style={{visibility: 'hidden'}}></img>
+                    <img src="/images/Neighbor.png" alt="Cynthia not here wtf" id="neighbor" style={{visibility: 'hidden'}}></img>
+                    <img src="/images/City_hall.png" alt="Cynthia not here wtf" id="city-hall" style={{visibility: 'hidden'}}></img>
+                    {/* Row two buildings */}
+                    {/* Row Three buildings (Just the library) */}
+                    {/* Row Four buildings (Just the park one) */}
+                    {/* Row Five buildings (NOT COLLEGE) */}
+                    {/* Row Six building (College has different left side than others) */}
+                </td>
+                <td id="center-column">20
+                    <canvas ref={canvasRef} width={MAX_WIDTH} height={MAX_HEIGHT + locations_module.TOP_BUFFER} id="game-canvas"/>
+                </td>
+                <td id="right-column">
+                    <p>Right column</p>
+                </td>
+            </tr>
         </table>
     );
 };
